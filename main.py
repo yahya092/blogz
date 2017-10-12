f7b11187a69e4ea39f9932d601d4516d4b7f5163
Yahya Alasady <yahya092@gmail.com>
2017-10-12 18:07:04 -0500

    added error meassages

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:yahya092@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'random'
 
 class Blog(db.Model):
 
@@ -20,16 +21,28 @@ class Blog(db.Model):
 
 @app.route('/newpost',methods=['POST','GET'])
 def newpost():
+    
+    title_error = ''
+    body_error = ''
 
     if request.method == "POST":
         new_title = request.form["newpost_title"]
         new_post = request.form['newpost']
-        new_entry = Blog(new_title,new_post)
-        db.session.add(new_entry)
-        db.session.commit()
+        
+        if new_title == '':
+            title_error = 'Please fill in the title'
 
-        return redirect('/blog?id=' + str(new_entry.id))
-
+        if new_post == '':
+            body_error = 'Plase fill in the body'
+            
+        if not title_error and not body_error:
+            new_entry = Blog(new_title,new_post)
+            db.session.add(new_entry)
+            db.session.commit()
+            return redirect('/blog?id=' + str(new_entry.id))
+        else:
+            return render_template("newpost.html",new_title=new_title, new_post= new_post, title_error=title_error,
+                body_error=body_error)
     return render_template('newpost.html')  
 
 @app.route('/blog',methods=['POST','GET'])
